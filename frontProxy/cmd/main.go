7a34a7301ca688ef8a7a534c64b0b2bb2cccfd93
student31415 <qwerty65k@mail.ru>
2022-12-10 21:58:20 +0300

    add calc client

--- frontProxy/cmd/main.go
+++ frontProxy/cmd/main.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/segmentio/kafka-go"
-	rpc "github.com/student31415/ComputerSystemsCource/calculator/pkg/calculator_pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
+	pb "main/pkg/calculator_pb"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -37,9 +40,25 @@ func main() {
 	router.POST("/work/:task_name/:work_name", Proxy(envs["MAIN_SERVICE_ADDRESS"], kafkaWriter))
 	router.POST("/work/:task_name", Proxy(envs["MAIN_SERVICE_ADDRESS"], kafkaWriter))
 	router.DELETE("/work/:task_name/:work_name", Proxy(envs["MAIN_SERVICE_ADDRESS"], kafkaWriter))
+	conn, err := grpc.Dial(envs["CALCULATOR_SERVICE_ADDRESS"]+"8090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	calc := pb.NewCalculatorClient(conn)
+
+	// Contact the server and print out its response.
 
 	router.GET("/calculate/:task_name", func(ctx *gin.Context) {
-		rpc.
+		task := ctx.Param("task_name")
+		ctx_rpc, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err := calc.Calculate(ctx_rpc, &pb.CalculateRequest{Task: task})
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("could not greet: %v", err))
+		}
+		ctx.JSON(http.StatusOK, gin.H{"task": task, "MinimalTime": r.GetTime()})
+
 	})
 
 	err = router.Run(":8080")
--- frontProxy/go.mod
+++ frontProxy/go.mod
@@ -5,6 +5,8 @@ go 1.18
 require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/segmentio/kafka-go v0.4.38
+	google.golang.org/grpc v1.51.0
+	google.golang.org/protobuf v1.28.0
 )
 
 require (
@@ -13,6 +15,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -23,9 +26,9 @@ require (
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-	golang.org/x/net v0.0.0-20220706163947-c90051bbdb60 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
