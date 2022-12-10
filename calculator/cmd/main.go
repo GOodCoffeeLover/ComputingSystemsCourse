3b814dfac75e19766f22909437cb684e72255aca
student31415 <qwerty65k@mail.ru>
2022-12-10 19:31:06 +0300

    mv calc

--- calculator/cmd/main.go
+++ calculator/cmd/main.go
@@ -4,9 +4,20 @@ import (
 	"calculator/internal/handlers"
 	"calculator/internal/storage"
 	"github.com/gin-gonic/gin"
+	pb "github.com/student31415/ComputingSystemsCourse/calculator"
 	"log"
 	"net/http"
 )
+
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
 
 func main() {
 	router := gin.Default()
